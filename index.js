bb6e3515e4900f4a503f3c18fd1c983f1ec251c5
Lucas de Oliveira <91337785+olucasoliveira@users.noreply.github.com>
2023-05-16 22:27:53 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -14,5 +14,63 @@ function realizarLogin() {
       email: email,
       senha: senha
     };
+
+    // Realizar o cadastro do usuário no banco de dados
+    // Implemente a lógica de inserção no banco de dados aqui
   
-   
+};
+
+function preencherEndereco() {
+    var cep = document.getElementById('cep').value;
+    if (cep.length === 8) {
+      fetch(`https://viacep.com.br/ws/${cep}/json/`)
+        .then(response => response.json())
+        .then(data => {
+          document.getElementById('endereco').value = data.logradouro;
+          document.getElementById('bairro').value = data.bairro;
+          document.getElementById('uf').value = data.uf;
+          document.getElementById('cidade').value = data.localidade;
+        })
+        .catch(error => {
+          console.error('Erro ao obter dados do CEP:', error);
+        });
+    }
+  }
+
+  function cadastrarUsuario() {
+    var nome = document.getElementById('nome').value;
+    var cpf = document.getElementById('cpf').value;
+    var rg = document.getElementById('rg').value;
+    var telefone = document.getElementById('telefone').value;
+    var cep = document.getElementById('cep').value;
+    var endereco = document.getElementById('endereco').value;
+    var numero = document.getElementById('numero').value;
+    var bairro = document.getElementById('bairro').value;
+    var uf = document.getElementById('uf').value;
+    var cidade = document.getElementById('cidade').value;
+
+    // Realizar o cadastro do usuário no banco de dados
+    // Implemente a lógica de inserção no banco de dados aqui
+    
+    // Exibir mensagem de sucesso ou erro
+    document.getElementById('error-message').innerHTML = 'Usuário cadastrado com sucesso!';
+  }
+
+  function cadastrarUsuario() {
+    var nome = document.getElementById('nome').value;
+    var email = document.getElementById('email').value;
+    var senha = document.getElementById('senha').value;
+
+    // Validar os campos obrigatórios
+    if (!nome || !email || !senha) {
+      document.getElementById('error-message').innerHTML = 'Por favor, preencha todos os campos.';
+      return;
+    }
+
+    // Realizar o cadastro do usuário no banco de dados
+    // Implemente a lógica de inserção no banco de dados aqui
+
+    // Exibir mensagem de sucesso ou erro
+    document.getElementById('error-message').innerHTML = 'Usuário cadastrado com sucesso!';
+  }
+
